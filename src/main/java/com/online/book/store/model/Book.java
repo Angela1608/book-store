9d96f3d39418b9d8e630c9ad796f9b6bd8d763b6
Anzhela Matiiva <89303267+Angela1608@users.noreply.github.com>
2023-11-30 23:52:19 +0200

    Update Book.java

--- src/main/java/com/online/book/store/model/Book.java
+++ src/main/java/com/online/book/store/model/Book.java
@@ -1,9 +1,9 @@
 package com.online.book.store.model;
 
-import java.math.BigDecimal;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
