c58ac06f28fba2254cc4291456ec9986a6858604
Angela Matiiva <angelamatiiva@gmail.com>
2023-12-01 12:31:21 +0200

    refactoring

--- src/main/java/com/online/book/store/dto/request/CreateBookRequestDto.java
+++ src/main/java/com/online/book/store/dto/request/CreateBookRequestDto.java
@@ -1,26 +1,16 @@
 package com.online.book.store.dto.request;
 
 import java.math.BigDecimal;
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+
     private String title;
-
-    @NotNull
     private String author;
-
-    @Column(unique = true)
     private String isbn;
-
-    @NotNull
     private BigDecimal price;
-
     private String description;
-
     private String coverImage;
 
 }
--- src/main/java/com/online/book/store/model/Book.java
+++ src/main/java/com/online/book/store/model/Book.java
@@ -1,6 +1,7 @@
 package com.online.book.store.model;
 
 import java.math.BigDecimal;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -18,11 +19,20 @@ public class Book {
     @Id
     private Long id;
 
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false, unique = true)
     private String author;
+
+    @Column(unique = true)
     private String isbn;
+
+    @Column(nullable = false)
     private BigDecimal price;
+
     private String description;
+
     private String coverImage;
 
 }
