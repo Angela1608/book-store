8f6001adff6c801f47fae476f007741d35947a7e
CodeAngel <89303267+Angela1608@users.noreply.github.com>
2023-12-19 16:23:14 +0200

    Update BookRequestDto.java

--- src/main/java/com/online/book/store/dto/request/BookRequestDto.java
+++ src/main/java/com/online/book/store/dto/request/BookRequestDto.java
@@ -1,10 +1,10 @@
 package com.online.book.store.dto.request;
 
-import java.math.BigDecimal;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
