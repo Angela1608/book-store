98c2cc2bdae3001f36997908617e0c48ceb4a045
CodeAngel <89303267+Angela1608@users.noreply.github.com>
2024-01-21 16:32:56 +0200

    Update SecurityConfig.java

--- src/main/java/com/online/book/store/config/SecurityConfig.java
+++ src/main/java/com/online/book/store/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.online.book.store.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
 import com.online.book.store.security.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
