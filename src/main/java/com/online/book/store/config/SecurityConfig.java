c947712357300d793c97ee9f1511edf71409be37
CodeAngel <89303267+Angela1608@users.noreply.github.com>
2024-01-21 16:26:55 +0200

    Update SecurityConfig.java

--- src/main/java/com/online/book/store/config/SecurityConfig.java
+++ src/main/java/com/online/book/store/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.online.book.store.config;
 
 import com.online.book.store.security.CustomUserDetailsService;
-import static org.springframework.security.config.Customizer.withDefaults;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableMethodSecurity
 @Configuration
